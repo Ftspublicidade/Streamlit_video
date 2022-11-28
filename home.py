6fc3d1359eeb022f4c4d71a1d3e9eaaeb21bfee8
Fernanda Santos <ftspublicidade@gmail.com>
2022-11-28 17:36:48 -0300

    Update home.py

--- home.py
+++ home.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import oci
-import pydub
+#import pydub
 #from pathlib import Path
 from datetime import date, datetime
 import ocifs
@@ -19,10 +19,10 @@ def main():
         audio_bytes = fileObject.read()
         st.video(audio_bytes, format='video/mp4')
 
-    if fileObject is not None:
-        if fileObject.name.endswith('wav'):
-            audio = pydub.AudioSegment.from_wav(fileObject)
-            file_type = 'wav'
+    #if fileObject is not None:
+        #if fileObject.name.endswith('wav'):
+            #audio = pydub.AudioSegment.from_wav(fileObject)
+            #file_type = 'wav'
 
         #save_path = Path("C:/Users/ffsantos/Documents/AI_Services/Speech_App")/fileObject.name
         #audio.export(save_path, format=file_type)

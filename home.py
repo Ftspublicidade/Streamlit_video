b195e5fec8979d9f62cddb9e7d3b6ef3947337ba
Fernanda Santos <ftspublicidade@gmail.com>
2022-11-28 17:33:18 -0300

    Update home.py

--- home.py
+++ home.py
@@ -42,26 +42,24 @@ def main():
         put_object_body = fileObject.getvalue(),
         content_type="video/mp4")
 
-
-def noti():
-    if main is not None:
-        config = oci.config.from_file("config")
-        ons_client = oci.ons.NotificationDataPlaneClient(config)
+    config1 = oci.config.from_file("config")
     
-        get_subscription_response = ons_client.get_subscription(
-        subscription_id="ocid1.onssubscription.oc1.sa-saopaulo-1.aaaaaaaajeyrwy7mjh546cvyvmpso4m5tzkmrevwhvqyahhhxctzqtnf24va",
-    )
+    ons_client = oci.ons.NotificationDataPlaneClient(config1)
+    
+    get_subscription_response = ons_client.get_subscription(
+    subscription_id="ocid1.onssubscription.oc1.sa-saopaulo-1.aaaaaaaajeyrwy7mjh546cvyvmpso4m5tzkmrevwhvqyahhhxctzqtnf24va",
+        )
 
-        st.text("Arquivo:")
-        st.write(get_subscription_response.data)
+    st.text("Arquivo:")
+    st.write(get_subscription_response.data)
+
+        
+    st.text("Arquivo:")
+    st.write(get_subscription_response.data)
     
   
         
         
-        
-            
-    
-
 main()
-noti()
+
 
